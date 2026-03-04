682ee97b7b5982c801b317047ffebb6fafe12eb3
sgoficialcoaching <sgoficialcoaching@gmail.com>
2026-03-04 07:51:14 +0100

    Updated package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+  plugins: [],
+  server: {
+    port: 3000
   }
 })
