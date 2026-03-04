4b010f2a39d39c714a4b618a6341f3caea0349a3
sgoficialcoaching <sgoficialcoaching@gmail.com>
2026-03-04 08:08:06 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [],
+  plugins: [react()],
   server: {
     port: 3000
-  },
-  optimizeDeps: {
-    exclude: ['*']
   }
 })
